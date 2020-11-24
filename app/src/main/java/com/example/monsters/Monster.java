c8ed56379abb1b5e43c225580d5c377a34409905
Gal-Or <galor.dvlp@gmail.com>
2020-11-24 10:14:24 +0200

    update

--- app/src/main/java/com/example/monsters/Monster.java
+++ app/src/main/java/com/example/monsters/Monster.java
@@ -5,6 +5,10 @@ public class Monster {
     private int id;
     private int power;
 
+    public Monster(){
+        this.id=-1;
+        this.power=-1;
+    }
 
     public Monster(int id, int power){
         this.id = id;
@@ -17,5 +21,13 @@ public class Monster {
 
     public int getPower(){
         return this.power;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
     }
 }
